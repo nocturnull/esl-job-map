58c37befa694144669ff56077ba08c75a0690c2d
Marcos Daniel Arroyo <mdarroyo0@gmail.com>
2018-06-14 04:57:23 +0900

    Added custom user model.

--- esljobmap/account/models.py
+++ esljobmap/account/models.py
@@ -1,3 +1,11 @@
+# account/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    name = models.CharField(blank=True, max_length=255)
+    email = models.EmailField(blank=False, max_length=255, help_text='Required. Provide a valid email address.')
+
+    def __str__(self):
+        return self.email
