ae1e8f218942a64d108a602c14b53c84b9580fe0
Marcos Daniel Arroyo <mdarroyo0@gmail.com>
2018-11-17 23:25:17 +0900

    Updated foundation version and made the mobile nav expanded by default.

--- esljobmap/static/foundation/gulpfile.js
+++ esljobmap/static/foundation/gulpfile.js
@@ -1,30 +1,31 @@
-var gulp = require('gulp');
+var gulp          = require('gulp');
 var webpack = require('webpack-stream');
 var minify = require('gulp-minify');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
-var $    = require('gulp-load-plugins')();
+var $             = require('gulp-load-plugins')();
+var autoprefixer  = require('autoprefixer');
 
 var sassPaths = [
-  'node_modules/foundation-sites/scss',
-  'node_modules/motion-ui/src'
+    'node_modules/foundation-sites/scss',
+    'node_modules/motion-ui/src'
 ];
 
-gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(rename('app.min.css'))
-    .pipe(gulp.dest('dist/'));
-});
+function sass() {
+    return gulp.src('scss/app.scss')
+        .pipe($.sass({
+            includePaths: sassPaths,
+            outputStyle: 'compressed' // if css compressed **file size**
+        })
+            .on('error', $.sass.logError))
+        .pipe($.postcss([
+            autoprefixer({ browsers: ['last 2 versions', 'ie >= 9'] })
+        ]))
+        .pipe(rename('app.min.css'))
+        .pipe(gulp.dest('dist/'));
+}
 
-gulp.task('vendor-js', function () {
+function vendorJs() {
     return gulp.src([
         'node_modules/jquery/dist/jquery.js',
         'node_modules/what-input/dist/what-input.js',
@@ -35,10 +36,9 @@ gulp.task('vendor-js', function () {
         }))
         .pipe(concat('vendor.min.js'))
         .pipe(gulp.dest('dist/'));
+}
 
-});
-
-gulp.task('site-js', function () {
+function siteJs() {
     return gulp.src('js/app.js')
         .pipe(webpack({
             output: {
@@ -47,8 +47,13 @@ gulp.task('site-js', function () {
             mode: 'production'
         }))
         .pipe(gulp.dest('dist/'));
-});
+}
 
-gulp.task('default', ['sass', 'vendor-js', 'site-js'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
-});
+function watch() {
+  gulp.watch("scss/*.scss", sass);
+}
+
+gulp.task('sass', sass);
+gulp.task('vendor-js', vendorJs);
+gulp.task('site-js', siteJs);
+gulp.task('default', gulp.parallel(['sass', 'vendor-js', 'site-js'], watch));
