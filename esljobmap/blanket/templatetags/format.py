8f75ed9342a0ae490dd0a2a6afefc6b1fc3e36f2
Marcos Daniel Arroyo <mdarroyo0@gmail.com>
2018-12-18 01:15:25 +0900

    Added contact number to My Applications and increased truncate limit for FlowCard content.

--- esljobmap/blanket/templatetags/format.py
+++ esljobmap/blanket/templatetags/format.py
@@ -7,6 +7,6 @@ register = template.Library()
 
 @register.filter
 def trunc(value):
-    if len(value) > 10:
-        return value[:9] + '<span data-tooltip title="{}">...</span>'.format(value)
+    if len(value) > 30:
+        return value[:30] + '<span data-tooltip title="{}">...</span>'.format(value)
     return value
