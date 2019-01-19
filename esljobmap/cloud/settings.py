5a598ef03fb663821dabe01acef490ff6085b410
Marcos Daniel Arroyo <mdarroyo0@gmail.com>
2019-01-19 11:22:00 +0900

    Fixed http link for standard cdn images.

--- esljobmap/cloud/settings.py
+++ esljobmap/cloud/settings.py
@@ -7,5 +7,5 @@ AWS_S3_BUCKET = os.environ.get('AWS_S3_BUCKET')
 AWS_S3_PARENT_DIR = os.environ.get('AWS_S3_PARENT_DIR', 'staging')
 AWS_DEFAULT_REGION = os.environ.get('AWS_DEFAULT_REGION')
 AWS_SECURE_CDN_BASE_LINK = 'https://cdn.esljobmap.com/'
-AWS_STANDARD_CDN_BASE_LINK = 'https://cdn.esljobmap.com/'
+AWS_STANDARD_CDN_BASE_LINK = 'http://cdn.esljobmap.com/'
 AWS_S3_BASE_LINK = 'https://s3.{0}.amazonaws.com/{1}/{2}'
