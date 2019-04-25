7c79d7f1069de7c567c084c481a43cea0173e402
Marcos Daniel Arroyo <mdarroyo0@gmail.com>
2019-04-25 21:00:42 +0900

    Add part time posting job credit consumption.

--- esljobmap/employment/mixins/recruiter.py
+++ esljobmap/employment/mixins/recruiter.py
@@ -1,12 +1,47 @@
 # employment/mixins/recruiter.py
 
+from job_credit.model_generators.history import RecordOriginator
+from employment.model_decorators.job_post import ArrayedJobPost
+from account.model_regulators.user import UserTransformer
+
 from django.http.response import HttpResponseForbidden
+from django.shortcuts import redirect, render
+from django.db import transaction
 
 
 class IsJobPosterMixin:
+    """Mixin to check job poster permissions."""
 
     def dispatch(self, request, *args, **kwargs):
         job_post = self.get_object()
         if job_post.is_job_poster(request.user):
             return super().dispatch(request, *args, **kwargs)
         return HttpResponseForbidden()
+
+
+class JobPostCreateMixin:
+    """Mixin for creating a job post."""
+
+    def create_response(self, request, form, is_full_time):
+        user = request.user
+        user_regulator = UserTransformer(user)
+        context = {'form': form, 'is_full_time': is_full_time}
+
+        if form.is_valid():
+            if user_regulator.can_afford_post():
+                arrayed_job = ArrayedJobPost(form.save(commit=False))
+                arrayed_job.normalize_location()
+
+                # Relevant job post, credits, and log need to be updated atomically.
+                with transaction.atomic():
+                    # Save new job post.
+                    arrayed_job.create(is_full_time=is_full_time, user=request.user)
+                    # Deduct credits from the users account.
+                    user_regulator.consume_post_credits()
+                    # Track the changes
+                    RecordOriginator.create_post_record(user, is_consumption=True, is_full_time=is_full_time)
+
+                return redirect(self.success_url)
+            else:
+                context['additional_error'] = 'Error: Insufficient job credits'
+        return render(request, self.template_name, context=context)
--- esljobmap/employment/views/job_post/create.py
+++ esljobmap/employment/views/job_post/create.py
@@ -1,22 +1,14 @@
 # employment/views/recruiter/job_create.py
 
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.db import transaction
 
 from employment.forms.job_post.create import CreateFullTimeJobForm, CreatePartTimeJobForm
-from employment.model_decorators.job_post import ArrayedJobPost
+from employment.mixins.recruiter import JobPostCreateMixin
 from employment.decorators import recruiter_required
-from employment.models import JobPost
-
-from account.model_regulators.user import UserTransformer
-
-from job_credit.model_generators.history import RecordOriginator
-
-from datetime import datetime
 
 
 @method_decorator(recruiter_required, name='dispatch')
@@ -28,7 +20,7 @@ class JobPostIndex(LoginRequiredMixin, TemplateView):
 
 
 @method_decorator(recruiter_required, name='dispatch')
-class CreateFullTimeJobPost(LoginRequiredMixin, TemplateView):
+class CreateFullTimeJobPost(LoginRequiredMixin, JobPostCreateMixin, TemplateView):
     """
     Job Post creation view.
     """
@@ -48,60 +40,42 @@ class CreateFullTimeJobPost(LoginRequiredMixin, TemplateView):
 
     def post(self, request, *args, **kwargs):
         """
-        Attempt to create a new job post.
+        Attempt to create a new full time job post.
 
         :param request:
         :param args:
         :param kwargs:
         :return:
         """
-        form = CreateFullTimeJobForm(request.POST)
-        user = request.user
-        user_regulator = UserTransformer(user)
-        context = {'form': form, 'is_full_time': True}
-
-        if form.is_valid():
-            if user_regulator.can_afford_post():
-                arrayed_job = ArrayedJobPost(form.save(commit=False))
-                arrayed_job.normalize_location()
-
-                # Relevant job post, credits, and log need to be updated atomically.
-                with transaction.atomic():
-                    # Save new job post.
-                    arrayed_job.create(is_full_time=True, user=request.user)
-                    # Deduct credits from the users account.
-                    user_regulator.consume_post_credits()
-                    # Track the changes
-                    RecordOriginator.create_post_record(user, is_consumption=True, is_full_time=True)
-
-                return redirect(self.success_url)
-            else:
-                context['additional_error'] = 'Error: Insufficient job credits'
-        return render(request, self.template_name, context=context)
+        return self.create_response(request, CreateFullTimeJobForm(request.POST), is_full_time=True)
 
 
 @method_decorator(recruiter_required, name='dispatch')
-class CreatePartTimeJobPost(LoginRequiredMixin, CreateView):
+class CreatePartTimeJobPost(LoginRequiredMixin, JobPostCreateMixin, TemplateView):
     """
     Job Post creation view.
     """
-    model = JobPost
-    form_class = CreatePartTimeJobForm
     template_name = 'map/index.html'
     success_url = reverse_lazy('employment_my_job_posts')
 
-    def form_valid(self, form):
-        new_job_post = form.save(commit=False)
-
-        # # Fix location if needed.
-        # if PostManager.has_existing_location(new_job_post):
-        #     PostManager.offset_location(new_job_post)
-
-        new_job_post.site_user = self.request.user
-        new_job_post.posted_at = datetime.now()
-        new_job_post.save()
-
-        return super().form_valid(form)
-
     def get(self, request, *args, **kwargs):
+        """
+        HTTP GET method simply redirects.
+
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
         return redirect(reverse_lazy('employment_part_time_map') + "#postAnchor")
+
+    def post(self, request, *args, **kwargs):
+        """
+        Attempt to create a new part time job post.
+
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        return self.create_response(request, CreatePartTimeJobForm(request.POST), is_full_time=False)
