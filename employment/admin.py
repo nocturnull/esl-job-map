28865365f617bd693fa5327c09de773d932d300d
Marcos Daniel Arroyo <mdarroyo0@gmail.com>
2019-06-16 23:15:49 +0900

    Removed filters from the admin job post page.

--- employment/admin.py
+++ employment/admin.py
@@ -1,45 +1,10 @@
 # employment/admin.py
 
-from datetime import date
-
 from django.db.models import Q
 from django.contrib import admin
 
 from .models import JobPost, JobApplication
 from .forms.job_post.update import AdminEditJobForm
-
-
-class CreateDateListFilter(admin.SimpleListFilter):
-    # Human-readable title which will be displayed in the
-    # right admin sidebar just above the filter options.
-    title = 'Date Posted'
-
-    # Parameter for the filter that will be used in the URL query.
-    parameter_name = 'created_at'
-
-    def lookups(self, request, model_admin):
-        """
-        Returns a list of tuples. The first element in each
-        tuple is the coded value for the option that will
-        appear in the URL query. The second element is the
-        human-readable name for the option that will appear
-        in the right sidebar.
-        """
-        return (
-            ('prelaunch', 'Pre-launch Posts'),
-            ('postlaunch', 'Post-launch Posts'),
-        )
-
-    def queryset(self, request, queryset):
-        """
-        Returns the filtered queryset based on the value
-        provided in the query string and retrievable via
-        `self.value()`.
-        """
-        if self.value() == 'prelaunch':
-            return queryset.filter(created_at__lt=date(2019, 1, 19))
-        if self.value() == 'postlaunch':
-            return queryset.filter(created_at__gte=date(2019, 1, 19))
 
 
 class ApplicantsFilter(admin.SimpleListFilter):
@@ -62,7 +27,6 @@ class ApplicantsFilter(admin.SimpleListFilter):
 class JobPostAdmin(admin.ModelAdmin):
     list_display = ['title', 'last_posted_date', 'site_user_email', 'job_type', 'status',
                     'repost_state', 'applicant_count']
-    list_filter = (CreateDateListFilter, )
     ordering = ['-posted_at']
     form = AdminEditJobForm
     readonly_fields = ('created_at',)
